8e9db0041a2d97e86b2434d34e0729e0cb9d416c
karlos1998 <fadeusz98@gmail.com>
2024-06-04 13:56:27 +0200

    create new project

---
+++ src/main/java/it/letscode/autocentrum_scraper/AutocentrumScraperApplication.java
@@ -0,0 +1,13 @@
+package it.letscode.autocentrum_scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocentrumScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocentrumScraperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/letscode/autocentrum_scraper/AutocentrumScraperApplicationTests.java
@@ -0,0 +1,13 @@
+package it.letscode.autocentrum_scraper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocentrumScraperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
